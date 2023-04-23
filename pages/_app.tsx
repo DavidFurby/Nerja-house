29d92b8123e5869208a21eb7e36062671e00bd14
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-04-23 14:10:03 +0200

    update to get mobileState

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,20 @@ import "../styles/globals.css";
 import ScreenContext from "../utils/context/ScreenContext";
 function MyApp({ Component, pageProps }) {
   let [isMobile, setIsMobile] = useState(false);
-
-const getMobileState = () => {
-  let mobileTemp = window.matchMedia("(max-width: 800px)");
-  if (mobileTemp) {
-    setIsMobile(mobileTemp.matches);
-  }
-};
-useEffect(() => {
-  window.addEventListener("resize", getMobileState);
-});
+  const getMobileState = () => {
+    let mobileTemp = window.matchMedia("(max-width: 800px)");
+    if (mobileTemp) {
+      setIsMobile(mobileTemp.matches);
+    }
+  };
+  useEffect(() => {
+    getMobileState();
+    window.addEventListener("resize", getMobileState);
+    
+    return () => {
+      window.removeEventListener("resize", getMobileState);
+    };
+  });
   return (
     <ScreenContext.Provider value={isMobile}>
       <Layout>
