bf180213336a1f1616b8e1f4bdabf14ea37cf519
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-04-24 18:03:27 +0200

    And now

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "./NavigationBar";
 import Head from "next/head";
-import BackgroundShapes from "./BackgroundShapes";
+import BackgroundShapes from "./background_shapes";
 import Footer from "./Footer";
 const Layout = ({ children }) => {
   return (
