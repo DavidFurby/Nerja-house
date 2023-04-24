2aa9d5b21e496eda3b184cf2e0f3bf24e8200307
David Furby <57434673+DavidFurby@users.noreply.github.com>
2023-04-24 18:01:03 +0200

    new test

--- components/backgroundShapes.tsx
+++ components/backgroundShapes.tsx
@@ -1,7 +1,7 @@
 import classes from "../styles/background_shapes.module.css";
 const BackgroundShapes = () => {
   return (
-    <div style={{overflow: "hidden"}}>
+    <div style={{ overflow: "hidden" }}>
       <div className={classes.firstShape} />
       <div className={classes.secondShape} />
     </div>
